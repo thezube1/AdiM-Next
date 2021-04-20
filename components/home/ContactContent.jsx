8b40bed05fa45a148dca72aae08b4528af65451e
Zubin Hydrie <zubin.hydrie@bush.edu>
2021-04-19 19:17:14 -0700

    Push Latest

--- components/home/ContactContent.jsx
+++ components/home/ContactContent.jsx
@@ -1,6 +1,52 @@
 import React, { Component } from "react";
 import { FaRegEnvelope } from "react-icons/fa";
 import { BsEnvelope } from "react-icons/bs";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+
+const MailIcon = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const variants = {
+    hidden: { pathLength: 0 },
+    visible: {
+      pathLength: 1,
+    },
+  };
+
+  return (
+    <motion.svg
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 2 }}
+      width="354"
+      height="244"
+      viewBox="0 0 354 244"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <motion.rect width="354" height="244" />
+      <motion.path
+        variants={variants}
+        id="contactSVG"
+        d="M2 51C2 51 164.837 96 179.234 96C193.631 96 352 51 352 51M1 31C1 14.4315 14.4315 1 31 1H323C339.569 1 353 14.4315 353 31V213C353 229.569 339.569 243 323 243H31C14.4314 243 1 229.569 1 213V31Z"
+        stroke="black"
+      />
+    </motion.svg>
+  );
+};
 
 class ContactContent extends Component {
   state = {};
@@ -16,20 +62,8 @@ class ContactContent extends Component {
             style={{ textDecoration: "none", color: "black" }}
           >
             <div id="contactEnvelope">
-              <svg
-                width="354"
-                height="244"
-                viewBox="0 0 354 244"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="354" height="244" />
-                <path
-                  id="contactSVG"
-                  d="M2 51C2 51 164.837 96 179.234 96C193.631 96 352 51 352 51M1 31C1 14.4315 14.4315 1 31 1H323C339.569 1 353 14.4315 353 31V213C353 229.569 339.569 243 323 243H31C14.4314 243 1 229.569 1 213V31Z"
-                  stroke="black"
-                />
-              </svg>
+              <MailIcon />
+
               <div id="contactMailText">adimahendru96@gmail.com</div>
             </div>
             {/*<div id="contactEnvelope">
